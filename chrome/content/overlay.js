0af9c8b9925ca9ddbbb35cef505d6f12f37549e7
Bernd <bmlk@gmx.de>
2012-02-23 22:13:25 +0100

    unescape path information

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -40,7 +40,7 @@ var filemail = {
     try {
       Components.classes['@mozilla.org/alerts-service;1']
               .getService(Components.interfaces.nsIAlertsService)
-              .showAlertNotification(null, title, text, false, '', null);
+              .showAlertNotification(null, title, unescape(text), false, '', null);
     } catch(e) {
       // prevents runtime error on platforms that don't implement nsIAlertsService
     }
